b642df1ce7681e8ffbdfc62d9caf7ccd05b25a75
rohseol <seven800@naver.com>
2022-11-16 22:45:20 +0900

    list api

--- my-app/src/todos/TodoList.js
+++ my-app/src/todos/TodoList.js
@@ -3,11 +3,25 @@ import TodoItem from './TodoItem';
 import './TodoList.css';
 
 const TodoList = ({todos,onDel,onToggle}) => {
+    const UrlList = (e) =>{
+        fetch(`http://163.180.117.22:8881/api/list`,{
+            method: 'GET'
+        })
+        .then(data => {
+            console.log(data)
+        })
+    }
     return (
+        // <ul className='TodoList'>
+        //     {
+        //         todos.map(todos=><TodoItem key={todos.id}
+        //         todos={todos} onDel={onDel} onToggle={onToggle}/>)
+        //     }
+        // </ul>
         <ul className='TodoList'>
             {
-                todos.map(todos=><TodoItem key={todos.id}
-                todos={todos} onDel={onDel} onToggle={onToggle}/>)
+                todos.map(todos=><UrlList key={todos.id}
+                todos={todos} />)
             }
         </ul>
     );
