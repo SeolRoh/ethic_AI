d2036886259025bdb07437af05ee702c72f37fb9
junguk <korea7791@naver.com>
2022-11-17 01:15:22 +0900

    imglist fetch

--- my-app/src/todos/TodoList.js
+++ my-app/src/todos/TodoList.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 import './TodoList.css';
+import { useState, useEffect } from "react";
 
 const TodoList = ({todos,onDel,onToggle}) => {
+    const [imgList, setImgList] = React.useState([]);
+
+    /*
     const UrlList = (e) =>{
         fetch(`http://163.180.117.22:8881/api/list`,{
             method: 'GET'
         })
         .then(data => {
-            console.log(data)
+            console.log(data.json());
         })
+    }*/
+
+    useEffect(() => {
+        fetch(`http://163.180.117.22:8881/api/list`,{
+            method: 'GET'
+        })
+        .then(response => {
+           return response.json();
+        })
+        .then(res => {
+            setImgList(res.data);
+        })
+        .catch(error => {
+            console.log(error);
+          })
+    },[setImgList]);
+
+    const onimgList = () => {
+        console.log("test");
+        console.log(imgList);
     }
+
     return (
         // <ul className='TodoList'>
         //     {
@@ -19,10 +44,8 @@ const TodoList = ({todos,onDel,onToggle}) => {
         //     }
         // </ul>
         <ul className='TodoList'>
-            {
-                todos.map(todos=><UrlList key={todos.id}
-                todos={todos} />)
-            }
+            {imgList.map((todoImg) =><img src={todoImg.url} />)}
+            <button onClick={onimgList}>클릭</button>
         </ul>
     );
 };
